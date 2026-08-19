b1895298a700392bcfa48c3e8f9f153e96b1f99c
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2026-08-19 21:49:25 +0200

    feat(player): Create VolumeSlider component that uses persisted volume settings

---
+++ src/mainview/components/VolumeSilder/usePersistedVolume.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+import { useDebounce } from "../../debounce"
+import { useUserSettingsContext } from "../../userSettings/userSettingsContext"
+
+export function usePersistedVolume() {
+	const {
+		userSettings,
+		updateUserSettings,
+		isLoaded: isUserSettingsLoaded,
+	} = useUserSettingsContext()
+
+	const [muted, setMuted] = useState(false)
+	const [volume, setVolume] = useState(userSettings.volume)
+	const debouncedVolume = useDebounce(volume, 500)
+
+	// load saved userSettings as initial volume
+	// biome-ignore lint/correctness/useExhaustiveDependencies: don't rerun when user settings update
+	useEffect(() => {
+		if (!isUserSettingsLoaded) return
+		setVolume(userSettings.volume)
+	}, [isUserSettingsLoaded])
+
+	// save the wanted volume to userSettings. Use debounce value for less disk writes
+	// biome-ignore lint/correctness/useExhaustiveDependencies: dont rerun on reloaded settings
+	useEffect(() => {
+		if (!isUserSettingsLoaded) return
+		updateUserSettings({ volume: debouncedVolume })
+	}, [debouncedVolume])
+
+	return {
+		volume,
+		setVolume,
+		muted,
+		toggleMute: () => setMuted((m) => !m),
+	}
+}
