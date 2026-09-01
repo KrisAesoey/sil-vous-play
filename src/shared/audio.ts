f68e24119328bbf7edd69aac6acaefccc0d1720d
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2026-09-01 18:30:11 +0200

    feat(library): Add functions for scanning libraries with multiple albums

--- src/shared/audio.ts
+++ src/shared/audio.ts
@@ -3,6 +3,8 @@ import { z } from "zod"
 export const AUDIO_FILE_FORMATS = ["flac", "mp3", "ogg"] as const
 export type AudioFileFormat = (typeof AUDIO_FILE_FORMATS)[number]
 
+// bun schemas for file valdiation
+
 const AudioFileFormatSchema = z.enum(AUDIO_FILE_FORMATS)
 
 const TrackFileSchema = z.object({
@@ -12,7 +14,12 @@ const TrackFileSchema = z.object({
 	track: z.number(),
 })
 
-export const MetadataSchema = z.object({
+const BaseMetadataScehma = z.object({
+	version: z.number(),
+})
+
+export const AlbumMetadataSchema = BaseMetadataScehma.extend({
+	type: z.literal("album"),
 	title: z.string(),
 	tracks: z.array(TrackFileSchema),
 	version: z.number(),
@@ -21,5 +28,24 @@ export const MetadataSchema = z.object({
 	year: z.string().optional(),
 })
 
+export const LibraryMetadataSchema = BaseMetadataScehma.extend({
+	type: z.literal("library"),
+	albumPaths: z.array(z.string()),
+})
+
+export const MetadataSchema = z.discriminatedUnion("type", [
+	AlbumMetadataSchema,
+	LibraryMetadataSchema,
+])
+
 export type TrackFile = z.infer<typeof TrackFileSchema>
+export type AlbumMetadata = z.infer<typeof AlbumMetadataSchema>
+export type LibraryMetadata = z.infer<typeof LibraryMetadataSchema>
 export type Metadata = z.infer<typeof MetadataSchema>
+
+// view types for rendering information
+
+export type AlbumEntry = {
+	album: AlbumMetadata
+	dir: string
+}
