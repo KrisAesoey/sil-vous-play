8799997f846e7b898abcab0137bcd2fd56055eb8
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2026-09-05 12:55:30 +0200

    feat(playback): Give playback sole ownership of state of play

--- src/mainview/playback/usePlayback.ts
+++ src/mainview/playback/usePlayback.ts
@@ -2,15 +2,35 @@ import { useState } from "react"
 
 export type NowPlaying = { albumDir: string; trackNumber: number }
 export type UsePlayback = {
+	isPlaying: boolean
 	nowPlaying: NowPlaying | undefined
-	setNowPlaying: (newPlaying: NowPlaying) => void
+	play: (track: NowPlaying) => void
+	pause: () => void
+	toggle: () => void
 }
 
+/**
+ * Owns the state of play. Keeps track of what is playing and if it is currently playing.
+ * Provides functions to update these two states.
+ */
 export function usePlayback(): UsePlayback {
+	const [isPlaying, setIsPlaying] = useState<boolean>(false)
 	const [nowPlaying, setNowPlaying] = useState<NowPlaying>()
 
+	const play = (track: NowPlaying) => {
+		setNowPlaying(track)
+		setIsPlaying(true)
+	}
+
+	const pause = () => setIsPlaying(false)
+
+	const toggle = () => setIsPlaying((prev) => !prev)
+
 	return {
+		isPlaying,
 		nowPlaying,
-		setNowPlaying,
+		play,
+		pause,
+		toggle,
 	}
 }
